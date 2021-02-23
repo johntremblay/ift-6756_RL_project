23894a6a4fe633bd9052b4f9929e26857d338a86
john <jonathan.tremblay.11@gmail.com>
2021-02-22 21:17:05 -0500

    testing

--- alphazero/main.py
+++ alphazero/main.py
@@ -13,7 +13,7 @@ args = dotdict({
     'tempThreshold': 15,        #
     'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
-    'numMCTSSims': 10,          # Number of games moves for MCTS to simulate.
+    'numMCTSSims': 2,          # Number of games moves for MCTS to simulate.
     'arenaCompare': 40,         # Number of games to play during arena play to determine if new net will be accepted.
     'cpuct': 1,
 
@@ -35,8 +35,8 @@ if __name__ == "__main__":
     if args.load_model:
         print("Load trainExamples from file")
         c.loadTrainExamples()
-    sys.setrecursionlimit(100000)
-    threading.stack_size(200000000)
-    thread = threading.Thread(target=c.learn())
-    thread.start()
-    # c.learn()
+    # sys.setrecursionlimit(100000)
+    # threading.stack_size(200000000)
+    # thread = threading.Thread(target=c.learn())
+    # thread.start()
+    c.learn()
