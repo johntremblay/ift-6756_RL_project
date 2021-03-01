eb1293acf696c7f0a42345e07590e3d866da9127
john <jonathan.tremblay.11@gmail.com>
2021-02-28 23:24:38 -0500

    arena

--- Arena.py
+++ Arena.py
@@ -56,7 +56,7 @@ class Arena():
                 log.debug(f'valids = {valids}')
                 assert valids[action] > 0
             board, curPlayer = self.game.getNextState(board, curPlayer, action)
-            board = board.mirror()
+            # board = board.mirror()
         if verbose:
             assert self.display
             print("Game over: Turn ", str(it), "Result ", str(self.game.getGameEnded(board, 1)))
