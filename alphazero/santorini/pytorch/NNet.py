d98aaa362f9c18acacf40379c46fee03ce1185e2
JOHN <jonathan.tremblay.11@gmail.com>
2021-03-14 23:28:16 -0400

    a

--- alphazero/santorini/pytorch/NNet.py
+++ alphazero/santorini/pytorch/NNet.py
@@ -25,9 +25,9 @@ args = dotdict({
     'lr': 0.001,
     'dropout': 0.1,
     'epochs': 20,
-    'batch_size': 128,
+    'batch_size': 1024,
     'cuda': torch.cuda.is_available(),
-    'num_channels': 512, #512 need 7.88 GB RAM free in GPU
+    'num_channels': 1024, #512 need 7.88 GB RAM free in GPU
 })
 
 
