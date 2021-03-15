96111ce6c16dba5f909240f815adb269154fc13c
JOHN <jonathan.tremblay.11@gmail.com>
2021-03-14 23:39:50 -0400

    :revert typo

--- alphazero/main.py
+++ alphazero/main.py
@@ -17,8 +17,8 @@ args = dotdict({
     'tempThreshold': 5,        #
     'updateThreshold': 0.5,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 2048,    # Number of game examples to train the neural networks.
-    'numMCTSSims': 10,          # Number of games moves for MCTS to simulate.
-    'arenaCompare': 10,         # Number of games to play during arena play to determine if new net will be accepted.
+    'numMCTSSims': 3,          # Number of games moves for MCTS to simulate.
+    'arenaCompare': 6,         # Number of games to play during arena play to determine if new net will be accepted.
     'cpuct': 1,
 
     'checkpoint': './temp/',
