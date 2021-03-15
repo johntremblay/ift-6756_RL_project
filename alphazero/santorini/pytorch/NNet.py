2655cae1e8bd34eebaaa194aeddde960f5a5abb6
JOHN <jonathan.tremblay.11@gmail.com>
2021-03-14 23:30:45 -0400

    changed config for V100 Tesla

--- alphazero/santorini/pytorch/NNet.py
+++ alphazero/santorini/pytorch/NNet.py
@@ -24,8 +24,8 @@ from .SantoriniNNet import SantoriniNNet as onnet
 args = dotdict({
     'lr': 0.001,
     'dropout': 0.1,
-    'epochs': 20,
-    'batch_size': 1024,
+    'epochs': 30,
+    'batch_size': 512,
     'cuda': torch.cuda.is_available(),
     'num_channels': 1024, #512 need 7.88 GB RAM free in GPU
 })
