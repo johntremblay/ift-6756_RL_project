b6ce244dab710403cc85e6810507d77d8e64adbd
JOHN <jonathan.tremblay.11@gmail.com>
2021-03-16 23:44:08 -0400

    boom clack

--- alphazero/santorini/pytorch/NNet.py
+++ alphazero/santorini/pytorch/NNet.py
@@ -24,10 +24,10 @@ from .SantoriniNNet import SantoriniNNet as onnet
 args = dotdict({
     'lr': 0.001,
     'dropout': 0.1,
-    'epochs': 2,
-    'batch_size': 2,
+    'epochs': 20,
+    'batch_size': 512,
     'cuda': torch.cuda.is_available(),
-    'num_channels': 8, #512 need 7.88 GB RAM free in GPU
+    'num_channels': 512, #512 need 7.88 GB RAM free in GPU
 })
 
 
